053390236ce2da7f9268a246c195920fe8db162c
HitKoladiya <koladiyahit45@gmail.com>
2023-10-19 20:49:41 +0530

    responsive in web with content dynamic

--- src/components/OurProducts.vue
+++ src/components/OurProducts.vue
@@ -1,10 +1,20 @@
 <script setup></script>
 
 <template>
-  <img id="web" src="../images/our.png" alt="" />
+   <br>
+    <br>
+  <div id="web">
+    <img  src="../images/our-above.png" alt="" />
+    <div class="popularHeading">
+      <h2><b>Our Popular Products</b></h2>
+      <div>
+        From the Harmonious union between a mixes of cereals flours and legumes flours, a new line of testy
+        specialitiesrich in fiber and vegetable proteins is born.
+      </div>
+    </div>
+    <img src="../images/our.png" alt="">
+  </div>
   <div id="mobile">
-    <br>
-    <br>
     <img src="../images/mobile-popularproduct-above.png" alt="">
     <div class="popularHeading">
       <h2><b>Our Popular Products</b></h2>
@@ -15,11 +25,14 @@
     </div>
     <img src="../images/mobile-popularproduct.png" alt="">
   </div>
+  <br>
+    <br>
 </template>
 
 <style scoped>
 img {
-  margin: 7rem auto;
+  /* margin: 7rem auto; */
+  margin: 0px;
   width: 100%;
   height: 100%;
 }
@@ -31,9 +44,7 @@ img {
 #mobile {
   display: none;
 }
-
-@media (max-width: 768px) {
-  .popularHeading {
+.popularHeading {
     background-color: #1C0B03 !important;
   }
 
@@ -49,7 +60,6 @@ img {
     /* 281.25% */
     letter-spacing: 0.64px;
   }
-
   .popularHeading div {
     color: var(--White, #FFF);
     text-align: center;
@@ -63,6 +73,9 @@ img {
     opacity: 0.6;
   }
 
+@media (max-width: 768px) {
+
+ 
   img {
     width: 100%;
     max-height: 800px;
